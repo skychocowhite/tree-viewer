5acc0231a45319d6b79a9bd0464cbf0f14363c20
skychocowhite <simon2000.wang@gmail.com>
2024-10-06 14:30:55 +0800

    [Update] Finish AST tree layout

--- src/app/main-window/display-tree/display-tree.component.ts
+++ src/app/main-window/display-tree/display-tree.component.ts
@@ -13,8 +13,8 @@ export class DisplayTreeComponent implements OnInit, AfterViewInit {
   @ViewChild('treeContainer') private readonly treeContainer!: ElementRef<HTMLElement>;
 
   // Tree layout and hierarchy structure
-  private hierarchyRoot!: d3.HierarchyNode<D3TreeNodeImpl>;
-  private tree!: d3.TreeLayout<D3TreeNodeImpl>;
+  private hierarchyRoot!: d3.HierarchyNode<D3TreeNodeWrapper>;
+  private tree!: d3.TreeLayout<D3TreeNodeWrapper>;
 
   // svg container, and two layers, one for nodes and one for links
   private svg!: d3.Selection<SVGSVGElement, undefined, null, undefined>;
@@ -24,16 +24,17 @@ export class DisplayTreeComponent implements OnInit, AfterViewInit {
   // Svg parameters
   private width: number = 3840;
   private height: number = 1080;
-  private margin: { top: number, bottom: number, left: number, right: number }
-    = { top: 10, bottom: 10, left: 150, right: 10 };
-  private dx!: number;
-  private dy!: number;
+  private readonly margin: { top: number, bottom: number, left: number, right: number }
+    = { top: 10, bottom: 10, left: 100, right: 10 };
+
+  // Use rectangular for each node, the width of rectangular depends on the length of text
+  private readonly rectHeight: number = 50;
 
   // AST tree data
-  private data: D3TreeNodeImpl;
+  private data: D3TreeNodeWrapper;
 
   constructor(private readonly astTreeService: AstTreeService) {
-    this.data = new D3TreeNodeImpl(new TreeNode(""));
+    this.data = new D3TreeNodeWrapper(new TreeNode(""));
   }
 
   ngOnInit(): void {
@@ -41,17 +42,20 @@ export class DisplayTreeComponent implements OnInit, AfterViewInit {
     this.createTree();
     this.update(null, this.hierarchyRoot);
 
+    // Update AST tree data
     this.astTreeService.getTreeRoot().subscribe((treeNode: TreeNode) => {
-      this.data = new D3TreeNodeImpl(treeNode);
+      this.data = new D3TreeNodeWrapper(treeNode);
       this.createTree();
       this.update(null, this.hierarchyRoot);
     });
   }
 
+  // The container should insert the svg container after both tags are rendered
   ngAfterViewInit(): void {
     this.treeContainer.nativeElement.appendChild(this.svg.node()!);
   }
 
+  // Create svg and g containers using d3.Selection
   private createSvg(): void {
     this.svg = d3.create("svg")
       .attr("class", "svg-container")
@@ -59,44 +63,48 @@ export class DisplayTreeComponent implements OnInit, AfterViewInit {
       .attr("height", this.height)
       .attr("viewBox", [-this.margin.left, -this.margin.top, this.width, 10]);
 
-    this.gNode = this.svg.append("g")
-      .attr("class", "gNode")
-      .attr("cursor", "pointer")
-      .attr("pointer-events", "all");
-
     this.gLink = this.svg.append("g")
       .attr("class", "gLink")
       .attr("fill", "none")
       .attr("stroke", "#555")
       .attr("stroke-width", 1.5)
       .attr("stroke-opacity", 0.4);
+
+    this.gNode = this.svg.append("g")
+      .attr("class", "gNode")
+      .attr("cursor", "pointer")
+      .attr("pointer-events", "all");
   }
 
+  // Create tree and hierarchy structure
   private createTree(): void {
-    this.hierarchyRoot = d3.hierarchy<D3TreeNodeImpl>(this.data, (d: D3TreeNodeImpl) => d.children);
-    this.dx = 20;
-    this.width = this.margin.left + this.margin.right + (this.hierarchyRoot.height + 1) * 300
-    this.dy = (this.width - this.margin.left - this.margin.right) / (1 + this.hierarchyRoot.height);
-    this.tree = d3.tree<D3TreeNodeImpl>().nodeSize([this.dx, this.dy]);
+    this.hierarchyRoot = d3.hierarchy<D3TreeNodeWrapper>(this.data, (d: D3TreeNodeWrapper) => d.children);
+    this.tree = d3.tree<D3TreeNodeWrapper>();
 
-    this.hierarchyRoot.data.preX = this.dy / 2;
+    this.hierarchyRoot.data.preX = 0;
     this.hierarchyRoot.data.preY = 0;
-    this.hierarchyRoot.descendants().forEach((d: d3.HierarchyNode<D3TreeNodeImpl>, id: number) => {
+    this.hierarchyRoot.descendants().forEach((d: d3.HierarchyNode<D3TreeNodeWrapper>, id: number) => {
       d.data.id = id;
       d.data.hierarchyChildren = d.children;
     })
   }
 
-  private update(event: Event | null, sourceData: d3.HierarchyNode<D3TreeNodeImpl>) {
+  // Update layout
+  private update(event: Event | null, sourceData: d3.HierarchyNode<D3TreeNodeWrapper>) {
     // Compute new tree layout
     this.tree(this.hierarchyRoot);
-    let nodes: d3.HierarchyNode<D3TreeNodeImpl>[] = this.hierarchyRoot.descendants().reverse();
-    let links: d3.HierarchyLink<D3TreeNodeImpl>[] = this.hierarchyRoot.links();
 
-    // Resize SVG and tree layout
-    let left: d3.HierarchyNode<D3TreeNodeImpl> = this.hierarchyRoot;
-    let right: d3.HierarchyNode<D3TreeNodeImpl> = this.hierarchyRoot;
-    this.hierarchyRoot.eachBefore((node: d3.HierarchyNode<D3TreeNodeImpl>) => {
+    // Get nodes and links of current layout
+    let nodes: d3.HierarchyNode<D3TreeNodeWrapper>[] = this.hierarchyRoot.descendants().reverse();
+    let links: d3.HierarchyLink<D3TreeNodeWrapper>[] = this.hierarchyRoot.links();
+
+    // Shift node position to folder-liked layout
+    this.updateNodeXY(nodes[nodes.length - 1], 0);
+
+    // Resize SVG size
+    let left: d3.HierarchyNode<D3TreeNodeWrapper> = this.hierarchyRoot;
+    let right: d3.HierarchyNode<D3TreeNodeWrapper> = this.hierarchyRoot;
+    this.hierarchyRoot.eachBefore((node: d3.HierarchyNode<D3TreeNodeWrapper>) => {
       if (node.x! < left.x!) {
         left = node;
       }
@@ -104,136 +112,182 @@ export class DisplayTreeComponent implements OnInit, AfterViewInit {
         right = node;
       }
     })
-    this.height = right.x! - left.x! + this.margin.top + this.margin.bottom;
-    this.width = this.margin.left + this.margin.right + (this.hierarchyRoot.height + 1) * 300
-    this.dy = (this.width - this.margin.left - this.margin.right) / (1 + this.hierarchyRoot.height);
-    this.tree.nodeSize([this.dx, this.dy]);
 
+    this.height = right.x! - left.x! + this.rectHeight + this.margin.top + this.margin.bottom;
+    this.width = this.margin.left + this.margin.right + this.getTreeHeight(nodes[nodes.length - 1], this.getRectWidth(nodes[nodes.length - 1].data.treeNode.name));
     this.svg.transition()
       .duration(250)
       .attr("height", this.height)
       .attr("width", this.width)
-      .attr("viewBox", `-${this.margin.left} ${left.x! - this.margin.top} ${this.width} ${this.height}`);
+      .attr("viewBox", `-${this.margin.left} ${left.x! - this.rectHeight - this.margin.top} ${this.width} ${this.height}`);
 
 
     // ========================
     // ===== Node Section =====
     // ========================
+    this.updateNodes(nodes, sourceData);
 
-    // Update nodes
-    let node: d3.Selection<SVGGElement, d3.HierarchyNode<D3TreeNodeImpl>, SVGGElement, undefined>
-      = this.gNode.selectAll<SVGGElement, d3.HierarchyNode<D3TreeNodeImpl>>("g.node")
-        .data(nodes, (d: d3.HierarchyNode<D3TreeNodeImpl>) => d.data.id);
+    // ========================
+    // ===== Link Section =====
+    // ========================
+    this.updateLinks(links, sourceData);
 
-    // Enter nodes at the parent's previous position
-    let nodeEnter: d3.Selection<SVGGElement, d3.HierarchyNode<D3TreeNodeImpl>, SVGGElement, undefined>
+
+    // Store new previous positions
+    this.hierarchyRoot.eachBefore((d: d3.HierarchyNode<D3TreeNodeWrapper>) => {
+      d.data.preX = d.x!;
+      d.data.preY = d.y!;
+    });
+  }
+
+  // Get current tree height
+  private getTreeHeight(node: d3.HierarchyNode<D3TreeNodeWrapper>, curHeight: number): number {
+    let newHeight = curHeight;
+
+    if (node.children) {
+      node.children.forEach((d: d3.HierarchyNode<D3TreeNodeWrapper>) => {
+        newHeight = Math.max(newHeight, curHeight + 30 + this.getRectWidth(d.data.treeNode.name));
+        newHeight = Math.max(newHeight, this.getTreeHeight(d, curHeight + 30 + this.getRectWidth(d.data.treeNode.name) / 2));
+      });
+    }
+
+    return newHeight;
+  }
+
+  // Update nodes when data changed
+  private updateNodes(nodes: d3.HierarchyNode<D3TreeNodeWrapper>[], sourceData: d3.HierarchyNode<D3TreeNodeWrapper>): void {
+    // Insert new nodes
+    let node: d3.Selection<SVGGElement, d3.HierarchyNode<D3TreeNodeWrapper>, SVGGElement, undefined>
+      = this.gNode.selectAll<SVGGElement, d3.HierarchyNode<D3TreeNodeWrapper>>("g.node")
+        .data(nodes, (d: d3.HierarchyNode<D3TreeNodeWrapper>) => d.data.id);
+
+    // Enter: nodes at the parent's previous position
+    let nodeEnter: d3.Selection<SVGGElement, d3.HierarchyNode<D3TreeNodeWrapper>, SVGGElement, undefined>
       = node.enter().append("g")
         .attr("class", "node")
-        .attr("transform", (d: d3.HierarchyNode<D3TreeNodeImpl>) => `translate(${sourceData.data.preY},${sourceData.data.preX})`)
-        .attr("fill-opacity", 0)
-        .attr("stroke-opacity", 0)
-        .on("click", (event: MouseEvent, d: d3.HierarchyNode<D3TreeNodeImpl>) => {
+        .attr("transform", (d: d3.HierarchyNode<D3TreeNodeWrapper>) => `translate(${sourceData.data.preY},${sourceData.data.preX})`)
+        .on("click", (event: MouseEvent, d: d3.HierarchyNode<D3TreeNodeWrapper>) => {
           d.data.isOpen = d.data.hierarchyChildren ? !d.data.isOpen : false;
           d.children = d.data.isOpen ? d.data.hierarchyChildren : undefined;
           this.update(event, d);
         });
 
-    nodeEnter.append("circle")
-      .attr("r", 2.5)
-      .attr("fill", (d: d3.HierarchyNode<D3TreeNodeImpl>) => d.data.hierarchyChildren ? "#555" : "#999")
-      .attr("stroke-width", 10);
-
+    nodeEnter.append("rect")
     nodeEnter.append("text")
-      .attr("dy", "0.31em")
-      .attr("x", -6)
-      .attr("text-anchor", "end")
+
+    // Update: transition nodes to their new position
+    let nodeUpdate: d3.Transition<SVGGElement, d3.HierarchyNode<D3TreeNodeWrapper>, SVGGElement, undefined>
+      = node.merge(nodeEnter)
+        .transition()
+        .duration(250)
+        .attr("transform", (d: d3.HierarchyNode<D3TreeNodeWrapper>) => `translate(${d.y},${d.x})`)
+        .attr("fill-opacity", 1)
+        .attr("stroke-opacity", 1);
+
+    nodeUpdate.select("rect")
+      .attr("width", (d) => this.getRectWidth(d.data.treeNode.name))
+      .attr("height", this.rectHeight)
+      .attr("x", (d) => -(this.getRectWidth(d.data.treeNode.name) / 2))
+      .attr("y", -this.rectHeight)
+      .attr("rx", 18)
+      .attr("fill", "white")
+      .attr("fill-opacity", 1)
+      .attr("stroke", (d: d3.HierarchyNode<D3TreeNodeWrapper>) => d.data.hierarchyChildren ? "#555" : "#999")
+      .attr("stroke-width", 2);
+
+    nodeUpdate.select("text")
       .text(d => d.data.treeNode.name)
+      .attr("font-family", "Consolas")
+      .attr("font-size", "15px")
+      .attr("y", -this.rectHeight / 2)
+      .attr("dy", "0.31em")
+      .attr("text-anchor", "middle")
       .attr("stroke-linejoin", "round")
       .attr("stroke-width", 3)
       .attr("stroke", "white")
       .attr("paint-order", "stroke");
 
-    // Update: Transition nodes to their new position
-    let nodeUpdate: d3.Transition<SVGGElement, d3.HierarchyNode<D3TreeNodeImpl>, SVGGElement, undefined>
-      = node.merge(nodeEnter)
-        .transition()
-        .duration(250)
-        .attr("transform", (d: d3.HierarchyNode<D3TreeNodeImpl>) => `translate(${d.y},${d.x})`)
-        .attr("fill-opacity", 1)
-        .attr("stroke-opacity", 1);
-
-    nodeUpdate.select("circle")
-      .attr("fill", (d: d3.HierarchyNode<D3TreeNodeImpl>) => d.data.hierarchyChildren ? "#555" : "#999");
-
-    nodeUpdate.select("text")
-      .text(d => d.data.treeNode.name);
-
-    // Exit: Transition exiting nodesto the parent's new position
-    let nodeExit: d3.Transition<SVGGElement, d3.HierarchyNode<D3TreeNodeImpl>, SVGGElement, undefined>
-      = node.exit<d3.HierarchyNode<D3TreeNodeImpl>>()
+    // Exit: transition exiting nodesto the parent's new position
+    let nodeExit: d3.Transition<SVGGElement, d3.HierarchyNode<D3TreeNodeWrapper>, SVGGElement, undefined>
+      = node.exit<d3.HierarchyNode<D3TreeNodeWrapper>>()
         .transition()
         .duration(250)
         .remove()
-        .attr("transform", (d: d3.HierarchyNode<D3TreeNodeImpl>) => `translate(${sourceData.y},${sourceData.x})`)
+        .attr("transform", (d: d3.HierarchyNode<D3TreeNodeWrapper>) => `translate(${sourceData.y},${sourceData.x})`)
         .attr("fill-opacity", 0)
         .attr("stoke-opacity", 0);
+  }
 
+  // Update links when data changed
+  private updateLinks(links: d3.HierarchyLink<D3TreeNodeWrapper>[], sourceData: d3.HierarchyNode<D3TreeNodeWrapper>): void {
+    let link: d3.Selection<SVGPathElement, d3.HierarchyLink<D3TreeNodeWrapper>, SVGGElement, undefined>
+      = this.gLink.selectAll<SVGPathElement, d3.HierarchyLink<D3TreeNodeWrapper>>("path.link")
+        .data(links, (d: d3.HierarchyLink<D3TreeNodeWrapper>) => d.target.data.id);
 
-    // ========================
-    // ===== Link Section =====
-    // ========================
-
-    let diagonal: d3.Link<any, d3.HierarchyLink<D3TreeNodeImpl>, d3.HierarchyNode<D3TreeNodeImpl>>
-      = d3.linkHorizontal<d3.HierarchyLink<D3TreeNodeImpl>, d3.HierarchyNode<D3TreeNodeImpl>>()
-        .x((d: d3.HierarchyNode<D3TreeNodeImpl>) => d.y!)
-        .y((d: d3.HierarchyNode<D3TreeNodeImpl>) => d.x!);
-
-    // Update links
-    let link: d3.Selection<SVGPathElement, d3.HierarchyLink<D3TreeNodeImpl>, SVGGElement, undefined>
-      = this.gLink.selectAll<SVGPathElement, d3.HierarchyLink<D3TreeNodeImpl>>("path")
-        .data(links, (d: d3.HierarchyLink<D3TreeNodeImpl>) => d.target.data.id);
-
-    // Enter links at the parent's previous position
-    let linkEnter: d3.Selection<SVGPathElement, d3.HierarchyLink<D3TreeNodeImpl>, SVGGElement, undefined>
+    // Enter: links at the parent's previous position
+    let linkEnter: d3.Selection<SVGPathElement, d3.HierarchyLink<D3TreeNodeWrapper>, SVGGElement, undefined>
       = link.enter().append("path")
-        .attr("d", (d: d3.HierarchyLink<D3TreeNodeImpl>) => {
-          let o: { x: number, y: number } = { x: sourceData.data.preX, y: sourceData.data.preY };
-          return diagonal(d, { source: o, target: o });
-        });
+        .attr("class", "link")
 
     // Update: transition links to their new position
-    let linkUpdate: d3.Transition<SVGPathElement, d3.HierarchyLink<D3TreeNodeImpl>, SVGGElement, undefined>
+    let linkUpdate: d3.Transition<SVGPathElement, d3.HierarchyLink<D3TreeNodeWrapper>, SVGGElement, undefined>
       = link.merge(linkEnter)
         .transition()
         .duration(250)
-        .attr("d", diagonal);
+        .attr("d", (d: d3.HierarchyLink<D3TreeNodeWrapper>) =>
+          `M${d.source.y},${d.source.x} V${d.target.x! - this.rectHeight / 2} H${d.target.y! - this.getRectWidth(d.target.data.treeNode.name) / 2}`)
+        .attr("fill-opacity", 1)
+        .attr("stroke-width", 5)
+        .attr("stroke-opacity", 1);
 
     // Exit: transition existing links to the parent's new position
-    let linkExit: d3.Transition<SVGPathElement, d3.HierarchyLink<D3TreeNodeImpl>, SVGGElement, undefined>
-      = link.exit<d3.HierarchyLink<D3TreeNodeImpl>>()
+    let linkExit: d3.Transition<SVGPathElement, d3.HierarchyLink<D3TreeNodeWrapper>, SVGGElement, undefined>
+      = link.exit<d3.HierarchyLink<D3TreeNodeWrapper>>()
         .transition()
         .duration(250)
         .remove()
-        .attr("d", (d: d3.HierarchyLink<D3TreeNodeImpl>) => {
-          let o: { x: number, y: number } = { x: sourceData.x!, y: sourceData.y! };
-          return diagonal(d, { source: o, target: o });
-        })
+        .attr("stroke-opacity", 0);
+  }
 
+  // Update nodes position
+  private updateNodeXY(node: d3.HierarchyNode<D3TreeNodeWrapper>, curX: number): number {
+    node.x = curX;
 
-    this.hierarchyRoot.eachBefore((d: d3.HierarchyNode<D3TreeNodeImpl>) => {
-      d.data.preX = d.x!;
-      d.data.preY = d.y!;
-    });
+    if (node.parent) {
+      node.y = node.parent.y! + 30 + this.getRectWidth(node.data.treeNode.name) / 2;
+    }
+
+    if (node.children) {
+      node.children.forEach((child: d3.HierarchyNode<D3TreeNodeWrapper>) => {
+        curX = this.updateNodeXY(child, curX + this.rectHeight + 15);
+      });
+    }
+
+    return curX;
+  }
+
+  // Get width of rectangular depedning on current text width
+  private getRectWidth(text: string) {
+    const canvas: HTMLCanvasElement = document.createElement('canvas');
+    const context: CanvasRenderingContext2D = canvas.getContext('2d')!;
+    context.font = `${15}px Consolas`;
+    const metrics: TextMetrics = context.measureText(text);
+    return metrics.width + 30;
   }
 }
 
 
-class D3TreeNodeImpl {
-  public id: number;
-  public isOpen: boolean;
-  public treeNode: TreeNode;
-  public children: D3TreeNodeImpl[];
-  public hierarchyChildren: d3.HierarchyNode<D3TreeNodeImpl>[] | undefined;
+// Class to wrap original AST tree data
+class D3TreeNodeWrapper {
+  public id: number;                     // Index in the hierarcy structure, using BFS ordering
+  public isOpen: boolean;                // Flag to open nodes
+  public treeNode: TreeNode;             // Original AST data
+  public children: D3TreeNodeWrapper[];  // Wrapper of children
+
+  // Copy of children after insert into hierarchy structure, used for changing layout depending on opened or closed state
+  public hierarchyChildren: d3.HierarchyNode<D3TreeNodeWrapper>[] | undefined;
+
+  // Previous positions for the node
   public preX: number;
   public preY: number;
 
@@ -247,7 +301,7 @@ class D3TreeNodeImpl {
     this.preY = 0;
 
     rootNode.children.forEach((node: TreeNode) => {
-      this.children.push(new D3TreeNodeImpl(node));
+      this.children.push(new D3TreeNodeWrapper(node));
     });
   }
 }
