401c2d2607beea41c6d55defa735f067c3dfe7b1
skychocowhite <simon2000.wang@gmail.com>
2024-10-19 01:47:43 +0800

    [Add] Add routes module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { MainWindowComponent } from './components/main-window/main-window.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: MainWindowComponent
+    },
+    {
+        path: "/upload",
+    },
+    {
+        path: "**",
+        redirectTo: "",
+        pathMatch: 'full'
+    }
+];
